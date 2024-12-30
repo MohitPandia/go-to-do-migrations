f2e336b393d3a4cef726f667941658eb9c741a19
Yash-Pandia(Mohit) <yash@zoop.one>
2024-12-30 11:46:14 +0530

    soft delete added

--- tables/categories.go
+++ tables/categories.go
@@ -3,12 +3,15 @@ package tables
 import (
 	"database/sql"
 	"time"
+
+	"gorm.io/gorm"
 )
 
 type Categories struct {
-	ID        int       `gorm:"column:category_id;primaryKey;autoIncrement"`
-	PID       sql.NullString    `gorm:"column:category_pid;unique;not null;type:varchar(40)"`
-	Name      string    `gorm:"column:name;not null;unique;type:varchar(100)"`
-	CreatedAt time.Time `gorm:"column:created_at;autoCreateTime"`
-	UpdatedAt time.Time `gorm:"column:updated_at;autoUpdateTime"`
+	ID        int            `gorm:"column:category_id;primaryKey;autoIncrement"`
+	PID       sql.NullString `gorm:"column:category_pid;unique;not null;type:varchar(40)"`
+	Name      string         `gorm:"column:name;not null;unique;type:varchar(100)"`
+	CreatedAt time.Time      `gorm:"column:created_at;autoCreateTime"`
+	UpdatedAt time.Time      `gorm:"column:updated_at;autoUpdateTime"`
+	DeletedAt gorm.DeletedAt `gorm:"column:deleted_at"` // Adds support for soft deletes
 }
