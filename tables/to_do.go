f2320b441993bc492eb3a4fb24d2394c98648001
Yash-Pandia(Mohit) <yash@zoop.one>
2024-12-30 17:15:24 +0530

    added PID in todo table

--- tables/to_do.go
+++ tables/to_do.go
@@ -9,6 +9,7 @@ import (
 
 type Todo struct {
 	ID          int            `gorm:"column:todo_id;primaryKey;autoIncrement"`
+	PID         sql.NullString `gorm:"column:todos_pid;unique;not null;type:varchar(40)"`
 	UserPID     sql.NullString `gorm:"column:user_pid;not null;type:varchar(40)"`
 	CategoryPID sql.NullString `gorm:"column:category_pid;not null;type:varchar(40)"`
 	Title       string         `gorm:"column:title;not null;type:varchar(200)"`
@@ -18,8 +19,4 @@ type Todo struct {
 	CreatedAt   time.Time      `gorm:"column:created_at;autoCreateTime"`
 	UpdatedAt   time.Time      `gorm:"column:updated_at;autoUpdateTime"`
 	DeletedAt   gorm.DeletedAt `gorm:"column:deleted_at"` // Adds support for soft deletes
-
-	// Foreign Keys
-	// User     Users     `gorm:"foreignKey:UserID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
-	// Category Categories `gorm:"foreignKey:CategoryID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL"`
 }
